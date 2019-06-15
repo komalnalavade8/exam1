48775bf20d00c8ab8c99aad8544166c089772a65
Tabassum <tabs22sultana@gmail.com>
2019-06-15 11:41:55 +0530

    Question1

---
+++ ASDMQ1/ASDMQ1/src/main/java/com/example/ASDMQ1/Asdmq1Application.java
@@ -0,0 +1,13 @@
+package com.example.ASDMQ1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asdmq1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asdmq1Application.class, args);
+	}
+
+}
---
+++ ASDMQ1/ASDMQ1/src/test/java/com/example/ASDMQ1/Asdmq1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ASDMQ1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Asdmq1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
